051e056b2435a0cda6dfbbcf4952105eb0686c3f
Dimas <31516927+dimas-prates@users.noreply.github.com>
2024-04-18 03:30:07 -0300

    rename file, add and configure instances + security groups

--- main.tf
+++ main.tf
@@ -7,14 +7,17 @@ terraform {
   }
 }
 
-#variables defined in the file "openstack_variables.tfvars"
 variable "auth_url" {}
+variable "region" {}
 variable "application_credential_id" {}
 variable "application_credential_secret" {}
-variable "region" {}
 variable "image_id" {}
-variable "flavor_name" {}
 variable "router_external_network_id" {}
+variable "flavor_name_compute" {}
+variable "flavor_name_controller" {}
+variable "flavor_name_storage" {}
+variable "flavor_name_firewall" {}
+variable "external_remote_ip" {}
 
 provider "openstack" {
   alias                         = "rc"
@@ -22,6 +25,13 @@ provider "openstack" {
   application_credential_id     = var.application_credential_id
   application_credential_secret = var.application_credential_secret
   region                        = var.region
+}
+
+# Create a new SSH key pair
+resource "openstack_compute_keypair_v2" "openstack-key" {
+  provider   = openstack.rc
+  name       = "openstack-key"
+  public_key = file("./openstack.key.pub")
 }
 
 # Create an object storage container with private access
@@ -34,11 +44,44 @@ resource "openstack_objectstorage_container_v1" "openstack-object-storage-contai
   force_destroy = true
 }
 
-# Create a new SSH key pair
-resource "openstack_compute_keypair_v2" "openstack-key" {
-  provider   = openstack.rc
-  name       = "openstack-key"
-  public_key = file("./openstack.key.pub")
+# Create a new security group allowing access
+resource "openstack_networking_secgroup_v2" "openstack-security-group-access" {
+  provider    = openstack.rc
+  name        = "openstack-security-group-access"
+  description = "openstack rules"
+}
+
+# Create a new security group allowing VPN
+resource "openstack_networking_secgroup_v2" "openstack-security-group-vpn" {
+  provider    = openstack.rc
+  name        = "openstack-security-group-vpn"
+  description = "openstack rules"
+}
+
+# Create security group rule for SSH
+resource "openstack_networking_secgroup_rule_v2" "ssh-security-rule" {
+  provider          = openstack.rc
+  security_group_id = openstack_networking_secgroup_v2.openstack-security-group-access.id
+  direction         = "ingress"
+  protocol          = "tcp"
+  port_range_min    = 22
+  port_range_max    = 22
+
+  remote_ip_prefix  = var.external_remote_ip
+  ethertype         = "IPv4"  # Specify the ethertype as "IPv4"
+}
+
+# Create security group rule for SSH
+resource "openstack_networking_secgroup_rule_v2" "vpn-security-rule" {
+  provider          = openstack.rc
+  security_group_id = openstack_networking_secgroup_v2.openstack-security-group-vpn.id
+  direction         = "ingress"
+  protocol          = "udp"
+  port_range_min    = 1194
+  port_range_max    = 1194
+
+  remote_ip_prefix  = var.external_remote_ip
+  ethertype         = "IPv4"  # Specify the ethertype as "IPv4"
 }
 
 # Create the first network
@@ -81,24 +124,6 @@ resource "openstack_networking_subnet_v2" "openstack-external-subnet" {
   }
 }
 
-# Create a new security group allowing SSH
-resource "openstack_networking_secgroup_v2" "openstack-security-group" {
-  provider    = openstack.rc
-  name        = "openstack-security-group"
-  description = "openstack rules"
-}
-
-# Define security group rules for SSH access
-resource "openstack_networking_secgroup_rule_v2" "ssh-security-rule" {
-  provider          = openstack.rc
-  security_group_id = openstack_networking_secgroup_v2.openstack-security-group.id
-  direction         = "ingress"
-  protocol          = "tcp"
-  port_range_min    = 22
-  port_range_max    = 22
-  remote_ip_prefix  = "0.0.0.0/0"
-  ethertype         = "IPv4" # Specify the ethertype as "IPv4"
-}
 
 # Create a router
 resource "openstack_networking_router_v2" "openstack-router" {
@@ -120,14 +145,14 @@ resource "openstack_networking_router_interface_v2" "external-subnet-interface" 
   subnet_id = openstack_networking_subnet_v2.openstack-external-subnet.id
 }
 
-# Launch instances
+# Create controllers instances
 resource "openstack_compute_instance_v2" "openstack-controller-instances" {
   provider        = openstack.rc
-  count           = 2
+  count           = 3
   name            = "controller-${count.index + 1}"
-  flavor_name     = var.flavor_name
+  flavor_name     = var.flavor_name_controller
   key_pair        = openstack_compute_keypair_v2.openstack-key.name
-  security_groups = [openstack_networking_secgroup_v2.openstack-security-group.name, "default"]
+  security_groups = [openstack_networking_secgroup_v2.openstack-security-group-access.name, "default"]
   user_data       = file("./cloud-config.yaml")
   network {
     name = openstack_networking_network_v2.openstack-internal-network.name
@@ -145,14 +170,14 @@ resource "openstack_compute_instance_v2" "openstack-controller-instances" {
   }
 }
 
-# Create additional compute instances
+# Create compute instances
 resource "openstack_compute_instance_v2" "openstack-compute-instances" {
   provider        = openstack.rc
   count           = 2
   name            = "compute-${count.index + 1}"
-  flavor_name     = var.flavor_name
+  flavor_name     = var.flavor_name_compute
   key_pair        = openstack_compute_keypair_v2.openstack-key.name
-  security_groups = [openstack_networking_secgroup_v2.openstack-security-group.name, "default"]
+  security_groups = [openstack_networking_secgroup_v2.openstack-security-group-access.name, "default"]
   user_data       = file("./cloud-config.yaml")
   network {
     name = openstack_networking_network_v2.openstack-internal-network.name
@@ -170,14 +195,39 @@ resource "openstack_compute_instance_v2" "openstack-compute-instances" {
   }
 }
 
-# Create deploy instance
-resource "openstack_compute_instance_v2" "deploy-instance" {
+# Create Storage instances
+resource "openstack_compute_instance_v2" "openstack-storage-instances" {
+  provider        = openstack.rc
+  count           = 2
+  name            = "storage-${count.index + 1}"
+  flavor_name     = var.flavor_name_storage
+  key_pair        = openstack_compute_keypair_v2.openstack-key.name
+  security_groups = [openstack_networking_secgroup_v2.openstack-security-group-access.name, "default"]
+  user_data       = file("./cloud-config.yaml")
+  network {
+    name = openstack_networking_network_v2.openstack-internal-network.name
+  }
+  network {
+    name = openstack_networking_network_v2.openstack-external-network.name
+  }
+  block_device {
+    uuid                  = var.image_id
+    source_type           = "image"
+    destination_type      = "volume"
+    boot_index            = 0
+    volume_size           = 350  # Adjust size as needed
+    delete_on_termination = true # Set this flag to delete the volume on instance termination
+  }
+}
+
+# Create VPN instance
+resource "openstack_compute_instance_v2" "vpn-instance" {
   provider        = openstack.rc
   count           = 1
-  name            = "deploy-1"
-  flavor_name     = "bc1-basic-2-4"
+  name            = "vpn-1"
+  flavor_name     = var.flavor_name_firewall
   key_pair        = openstack_compute_keypair_v2.openstack-key.name
-  security_groups = [openstack_networking_secgroup_v2.openstack-security-group.name, "default"]
+  security_groups = [openstack_networking_secgroup_v2.openstack-security-group-access.name, openstack_networking_secgroup_v2.openstack-security-group-vpn.name, "default"]
   user_data       = file("./cloud-config.yaml")
   network {
     name = openstack_networking_network_v2.openstack-internal-network.name
